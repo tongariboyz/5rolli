491c65d22a54df91dedda7bb88a6dd03c7f24ad6
ideyuta <ide.yuta@gmail.com>
2015-11-26 11:40:29 +0900

    feat(reducers): story reducer にダミーデータを追加

--- src/js/renderer/reducers/story.js
+++ src/js/renderer/reducers/story.js
@@ -19,6 +19,29 @@ type Action = {
  * @return {Object}
  */
 export default function story(state: State = {
+  issues: [
+    {
+      id: '1',
+      summary: {
+        current: 2,
+        past: 1,
+        open: 2,
+        close: 2,
+        wait: 2
+      },
+      title: 'Issue 1'
+    }, {
+      id: '2',
+      summary: {
+        current: 0,
+        past: 0,
+        open: 2,
+        close: 0,
+        wait: 5
+      },
+      title: 'Issue 2'
+    }
+  ],
   stories: []
 }, action: Action): State {
   switch (action.type) {
