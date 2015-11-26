bde70d1a30de867c69c57724b975370a4c477e9f
ideyuta <ide.yuta@gmail.com>
2015-11-27 08:39:41 +0900

    feat(containers): StoryView コンポーネントを追加

---
+++ src/js/renderer/containers/StoryView.js
@@ -0,0 +1,54 @@
+/* @flow */
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import Story from '../components/Story';
+
+const propTypes = {
+  story: PropTypes.object.isRequired
+};
+
+
+class StoryView extends React.Component {
+  /**
+   * render Stories
+   *
+   * @param {Object[]} stories story object list
+   * @return {ReactElement[]}
+   */
+  renderStories(stories): React.Element {
+    if (stories.length !== 0) {
+      return stories.map((story, key) => {
+        return (
+          <Story
+            key={key}
+            story={story}
+          >
+            {this.renderStories(story.children)}
+          </Story>
+        );
+      });
+    }
+  }
+
+  /**
+   * render
+   *
+   * @return {ReactElement}
+   */
+  render(): React.Element {
+    return <div className="StoryView">{this.renderStories(this.props.story.stories)}</div>;
+  }
+}
+
+/**
+ * state を整形
+ *
+ * @param {Object} state state
+ * @return {Object}
+ */
+function mapStateToProps(state) {
+  return {story: state.story};
+}
+
+StoryView.propTypes = propTypes;
+export default connect(mapStateToProps)(StoryView);
