db13334a04cfda8a1e6dc4d3a06ec50004fe8640
ideyuta <ide.yuta@gmail.com>
2015-11-26 11:38:47 +0900

    feat(containers): IssueListView コンポーネントを追加

---
+++ src/js/renderer/containers/IssueListView.js
@@ -0,0 +1,49 @@
+/* @flow */
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import Issue from '../components/Issue';
+
+const propTypes = {
+  story: PropTypes.object.isRequired
+};
+
+
+class IssueListView extends React.Component {
+  /**
+   * render Issues
+   *
+   * @return {ReactElement}
+   */
+  renderIssues(): React.Element {
+    return this.props.story.issues.map((issue, key) => {
+      return (
+        <Issue
+          issue={issue}
+          key={key}
+        />
+      );
+    });
+  }
+
+  /**
+   * render
+   *
+   * @return {ReactElement}
+   */
+  render(): React.Element {
+    return <div className="IssueLiseView">{this.renderIssues()}</div>;
+  }
+}
+
+/**
+ * state を整形
+ *
+ * @param {Object} state state
+ * @return {Object}
+ */
+function mapStateToProps(state) {
+  return {story: state.story};
+}
+
+IssueListView.propTypes = propTypes;
+export default connect(mapStateToProps)(IssueListView);
