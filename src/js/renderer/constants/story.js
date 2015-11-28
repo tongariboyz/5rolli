12d463189fe54adb6ca822cb9dcca90c4501fdc6
moqada <moqada@gmail.com>
2015-11-28 21:27:08 +0900

    feat(constants): Story系の定数ファイルを追加

---
+++ src/js/renderer/constants/story.js
@@ -0,0 +1,13 @@
+/* @flow */
+export const STORY_TYPE = {
+  issue: 'issue',
+  story: 'story',
+  invalid: 'invalid'
+};
+
+export const STORY_STATUS = {
+  open: 'open',
+  close: 'close',
+  waiting: 'waiting',
+  unknown: 'unknown'
+};
