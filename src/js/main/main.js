d94803136baa40f57509976b7b2135299806889e
moqada <moqada@gmail.com>
2015-12-01 12:52:09 +0900

    feat(main): 設定ファイル読み込みIPC連携追加

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,14 +1,21 @@
 import path from 'path';
 
 import app from 'app';
+import {ipcMain} from 'electron';
 import CrashReporter from 'crash-reporter';
 import BrowserWindow from 'browser-window';
+
+import {loadConfig} from './config';
+import ipcTypes from '../common/ipcTypes';
 
 
 let mainWindow = null;
 CrashReporter.start();
 
-app.on('ready', () => {
+/**
+ * メイン処理の実行
+ */
+function run() {
   // メイン画面のサイズ
   mainWindow = new BrowserWindow({width: 800, height: 600});
   // 起動 url を指定
@@ -22,7 +29,17 @@ app.on('ready', () => {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
-});
+
+  // 設定ファイル読み込みリクエストを受けて読み込み結果を別Eventで返す
+  // TODO: IPC系は別に切り出したい
+  ipcMain.on(ipcTypes.CONFIG_LOAD_REQUEST, ev => {
+    loadConfig().then(config => {
+      ev.sender.send(ipcTypes.CONFIG_LOADED, config);
+    });
+  });
+}
+
+app.on('ready', () => run());
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
